382d1e0c0eab9767d783af1bf08528027e50519a
Jeremy Li <mail.jeremy.li@gmail.com>
2019-05-16 18:52:00 +1000

    updated to ngrx-8 part 2

--- src/app/party-management/reducers/adventurer-classes.reducer.ts
+++ src/app/party-management/reducers/adventurer-classes.reducer.ts
@@ -1,12 +1,10 @@
 import { AdventurerClassState } from '../state/adventurer-classes.state';
 import { adventurerClasses } from '../models/adventurer-class/adventurer-classes.const';
-import { Action } from '@ngrx/store';
+import { createReducer } from '@ngrx/store';
 
 const initialState: AdventurerClassState = {
   classes: adventurerClasses,
 };
 
-export function adventurerClassesReducer(state = initialState, _: Action) {
-  // no actions
-  return state;
-}
+// no actions
+export const adventurerClassesReducer = createReducer<AdventurerClassState>(initialState);
